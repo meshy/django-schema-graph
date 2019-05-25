285816646f8f8fd7ea5de164e1a49df576dac60d
Charlie Denton <charlie@meshy.co.uk>
2019-05-25 13:30:45 +0100

    Use top-level functions for tests
    There's no need to wrap these tests in a class.

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,71 +1,73 @@
-from django.test import TestCase
-
 from schema_graph.schema import get_schema
 
 
-class TestGetSchema(TestCase):
-    def test_nodes(self):
-        expected = [
-            ("django.contrib.auth", "Group"),
+def test_models():
+    expected = [
+        ("django.contrib.auth", "Group"),
+        ("django.contrib.auth", "Permission"),
+        ("django.contrib.auth", "User"),
+        ("django.contrib.contenttypes", "ContentType"),
+        ("django.contrib.sessions", "Session"),
+        ("django.contrib.sites", "Site"),
+        ("tests", "AnotherOneToOne"),
+        ("tests", "Concrete"),
+        ("tests", "NoOutgoingConnections"),
+        ("tests", "OutgoingForeignKey"),
+        ("tests", "OutgoingManyToMany"),
+        ("tests", "OutgoingOneToOne"),
+        ("tests", "ProxyNode"),
+        ("tests", "ProxyNode2"),
+        ("tests", "SelfReference"),
+        ("tests", "SubSubclass"),
+        ("tests", "Subclass"),
+        ("tests", "Subclass2"),
+    ]
+    assert get_schema().models == expected
+
+
+def test_foreign_key():
+    expected = [
+        (
             ("django.contrib.auth", "Permission"),
-            ("django.contrib.auth", "User"),
             ("django.contrib.contenttypes", "ContentType"),
-            ("django.contrib.sessions", "Session"),
-            ("django.contrib.sites", "Site"),
-            ("tests", "AnotherOneToOne"),
-            ("tests", "Concrete"),
-            ("tests", "NoOutgoingConnections"),
-            ("tests", "OutgoingForeignKey"),
-            ("tests", "OutgoingManyToMany"),
-            ("tests", "OutgoingOneToOne"),
-            ("tests", "ProxyNode"),
-            ("tests", "ProxyNode2"),
-            ("tests", "SelfReference"),
-            ("tests", "SubSubclass"),
-            ("tests", "Subclass"),
-            ("tests", "Subclass2"),
-        ]
-        assert get_schema().models == expected
+        ),
+        (("tests", "Concrete"), ("tests", "NoOutgoingConnections")),
+        (("tests", "OutgoingForeignKey"), ("tests", "NoOutgoingConnections")),
+        (("tests", "SelfReference"), ("tests", "SelfReference")),
+    ]
+    assert get_schema().foreign_keys == expected
 
-    def test_foreign_key(self):
-        expected = [
-            (
-                ("django.contrib.auth", "Permission"),
-                ("django.contrib.contenttypes", "ContentType"),
-            ),
-            (("tests", "Concrete"), ("tests", "NoOutgoingConnections")),
-            (("tests", "OutgoingForeignKey"), ("tests", "NoOutgoingConnections")),
-            (("tests", "SelfReference"), ("tests", "SelfReference")),
-        ]
-        assert get_schema().foreign_keys == expected
 
-    def test_one_to_one(self):
-        expected = [
-            (("tests", "AnotherOneToOne"), ("tests", "NoOutgoingConnections")),
-            (("tests", "OutgoingOneToOne"), ("tests", "NoOutgoingConnections")),
-        ]
-        assert get_schema().one_to_ones == expected
+def test_one_to_one():
+    expected = [
+        (("tests", "AnotherOneToOne"), ("tests", "NoOutgoingConnections")),
+        (("tests", "OutgoingOneToOne"), ("tests", "NoOutgoingConnections")),
+    ]
+    assert get_schema().one_to_ones == expected
 
-    def test_many_to_many(self):
-        expected = [
-            (("django.contrib.auth", "Group"), ("django.contrib.auth", "Permission")),
-            (("django.contrib.auth", "User"), ("django.contrib.auth", "Group")),
-            (("django.contrib.auth", "User"), ("django.contrib.auth", "Permission")),
-            (("tests", "OutgoingManyToMany"), ("tests", "NoOutgoingConnections")),
-        ]
-        assert get_schema().many_to_manys == expected
 
-    def test_inheritance(self):
-        expected = [
-            (("tests", "SubSubclass"), ("tests", "Subclass")),
-            (("tests", "Subclass"), ("tests", "NoOutgoingConnections")),
-            (("tests", "Subclass2"), ("tests", "OutgoingForeignKey")),
-        ]
-        assert get_schema().inheritance == expected
+def test_many_to_many():
+    expected = [
+        (("django.contrib.auth", "Group"), ("django.contrib.auth", "Permission")),
+        (("django.contrib.auth", "User"), ("django.contrib.auth", "Group")),
+        (("django.contrib.auth", "User"), ("django.contrib.auth", "Permission")),
+        (("tests", "OutgoingManyToMany"), ("tests", "NoOutgoingConnections")),
+    ]
+    assert get_schema().many_to_manys == expected
 
-    def test_proxy(self):
-        expected = [
-            (("tests", "ProxyNode"), ("tests", "OutgoingManyToMany")),
-            (("tests", "ProxyNode2"), ("tests", "OutgoingOneToOne")),
-        ]
-        assert get_schema().proxies == expected
+
+def test_inheritance():
+    expected = [
+        (("tests", "SubSubclass"), ("tests", "Subclass")),
+        (("tests", "Subclass"), ("tests", "NoOutgoingConnections")),
+        (("tests", "Subclass2"), ("tests", "OutgoingForeignKey")),
+    ]
+    assert get_schema().inheritance == expected
+
+
+def test_proxy():
+    expected = [
+        (("tests", "ProxyNode"), ("tests", "OutgoingManyToMany")),
+        (("tests", "ProxyNode2"), ("tests", "OutgoingOneToOne")),
+    ]
+    assert get_schema().proxies == expected
