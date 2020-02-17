ed65188db33f273ef0e78cb79a116b66038bf8f1
Charlie Denton <charlie@meshy.co.uk>
2020-02-17 23:28:26 +0000

    Use "prod" for yarn "build", and "dev" for "watch"

--- webpack.common.js
+++ webpack.common.js
@@ -6,9 +6,6 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
   context: __dirname,
 
-  mode: 'development',
-  devtool: 'source-map',
-
   entry: './assets/entrypoints/index.js',
 
   output: {
