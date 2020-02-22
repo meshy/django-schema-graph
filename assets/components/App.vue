6f1410fb68477ba2a6e519d45eb05d0627ec8457
Charlie Denton <charlie@meshy.co.uk>
2020-02-22 08:23:34 +0000

    Render once the map has finished stabilizing

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -4,6 +4,7 @@
       fixed left top fab small
       @click="sidebar = true"
       color="#64B5F6"
+      v-if="loaded"
       >
       <svg style="width:24px;height:24px" viewBox="0 0 24 24">
         <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
@@ -68,6 +69,9 @@ const options = {
 };
 
 
+let loaded = false;
+
+
 export default {
   name: 'App',
   components: {Network, VBtn, VNavigationDrawer},
@@ -81,6 +85,7 @@ export default {
     stabilizationIterationsDone: function () {
       console.log('Stabilization complete');
       this.$Progress.finish();
+      this.loaded = true;
     }
   },
   data() {
@@ -116,6 +121,7 @@ export default {
     ];
 
     return {
+      loaded,
       nodes,
       edges,
       options,
