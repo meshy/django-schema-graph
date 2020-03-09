2d2507eaa7179f51587ae0f578ddac695dffd11f
Charlie Denton <charlie@meshy.co.uk>
2020-03-09 21:49:28 +0000

    Add missing trove classifier
    Python 3.8 is tested, so it should be in this list.

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Database",
         "Topic :: Documentation",
         "Topic :: Utilities",
