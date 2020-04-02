6c60db2629bbaab49ef253e5619db24f411357c2
Marco De Felice <marco.defelice@virtualstock.co.uk>
2020-04-02 15:00:58 +0200

    Open the django admin doc model on double click

--- tests/urls.py
+++ tests/urls.py
@@ -1,13 +1,22 @@
+from django.contrib import admin
 from schema_graph.views import Schema
 
 
 try:
     # Django 2+:
-    from django.urls import path
+    from django.urls import path, include
 
-    urlpatterns = [path("", Schema.as_view())]
+    urlpatterns = [
+        path('admin/doc/', include('django.contrib.admindocs.urls')),
+        path('admin/', admin.site.urls),
+        path("", Schema.as_view())
+    ]
 except ImportError:
     # Django < 2:
     from django.conf.urls import url
 
-    urlpatterns = [url(r"^$", Schema.as_view())]
+    urlpatterns = [
+        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+        url('^admin/', admin.site.urls),
+        url(r"^$", Schema.as_view())
+    ]
