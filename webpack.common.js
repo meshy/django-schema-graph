40e975166ea01db8b78df97b144a8b9ba15e886e
Charlie Denton <charlie@meshy.co.uk>
2022-05-04 23:31:09 +0100

    Remove fibers
    It's no longer maintained, and is not compatible with newer versions of
    Node.
    
    Ref: https://sass-lang.com/blog/node-fibers-discontinued

--- webpack.common.js
+++ webpack.common.js
@@ -29,7 +29,6 @@ module.exports = {
             options: {
               implementation: require('sass'),
               sassOptions: {
-                fiber: require('fibers'),
                 indentedSyntax: true
               },
             },
