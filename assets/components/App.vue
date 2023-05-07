0f08106e0637bd222a3930a8c08d4a09d6093551
Charlie Denton <charlie@meshy.co.uk>
2023-05-07 23:48:32 +0100

    Move graph-editing sidebar into its own file

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,107 +1,15 @@
 <template>
   <div class="main-app">
     <v-app>
-      <v-btn fixed left top fab small
-        color="#64B5F6"
-        v-if="loaded"
-        @click="sidebar = true"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-      <v-navigation-drawer app temporary v-model="sidebar">
-
-        <v-toolbar flat>
-          <v-tooltip bottom attach=".main-app">
-            <template v-slot:activator="{ on }">
-              <v-btn small v-on="on" @click="hideAll">
-                <v-icon>mdi-eye-off</v-icon>
-              </v-btn>
-            </template>
-            <span>Hide all</span>
-          </v-tooltip>
-          <v-spacer></v-spacer>
-          <v-tooltip bottom attach=".main-app">
-            <template v-slot:activator="{ on }">
-              <v-btn small v-on="on" @click="showAll">
-                <v-icon>mdi-eye</v-icon>
-              </v-btn>
-            </template>
-            <span>Show all</span>
-          </v-tooltip>
-          <v-spacer></v-spacer>
-          <v-tooltip bottom attach=".main-app">
-            <template v-slot:activator="{ on }">
-              <v-btn small v-on="on" @click="collapseAll">
-                <v-icon>mdi-arrow-collapse-all</v-icon>
-              </v-btn>
-            </template>
-            <span>Collapse all</span>
-          </v-tooltip>
-          <v-spacer></v-spacer>
-          <v-tooltip bottom attach=".main-app">
-            <template v-slot:activator="{ on }">
-              <v-btn small v-on="on" @click="expandAll">
-                <v-icon>mdi-arrow-expand-all</v-icon>
-              </v-btn>
-            </template>
-            <span>Expand all</span>
-          </v-tooltip>
-        </v-toolbar>
-
-        <v-list expand dense>
-          <v-list-group
-            v-for="app in Object.keys(activeModels)"
-            :key="app"
-            v-model:value="activeModels[app].expanded"
-            :color="activeModels[app].visible ? activeModels[app].hardColor : inactiveColor"
-          >
-
-            <template v-slot:activator>
-              <v-list-item-icon
-                @click.stop="activeModels[app].visible = !activeModels[app].visible"
-              >
-                <v-icon
-                  v-if="activeModels[app].visible"
-                  v-bind:style="{color: activeModels[app].visible ? activeModels[app].hardColor : inactiveColor}"
-                >mdi-eye-outline</v-icon>
-                <v-icon v-else
-                  v-bind:style="{color: activeModels[app].visible ? activeModels[app].hardColor : inactiveColor}"
-                >mdi-eye-off-outline</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title
-                  v-text="app"
-                  v-bind:style="{color: activeModels[app].visible ? activeModels[app].hardColor : inactiveColor}"
-                ></v-list-item-title>
-              </v-list-item-content>
-            </template>
-            <v-list-item dense link
-              class="menu-model"
-              v-for="model, modelIndex in activeModels[app].models"
-              :key="model.id"
-              :disabled="!activeModels[app].visible"
-              @click="model.active = !model.active"
-            >
-              <v-list-item-content>
-                <v-list-item-title v-text="model.label"></v-list-item-title>
-              </v-list-item-content>
-              <v-list-item-action>
-                <v-icon v-if="!activeModels[app].visible && model.active">
-                  mdi-eye-outline
-                </v-icon>
-                <v-icon v-else-if="activeModels[app].visible && model.active" :color="activeModels[app].hardColor" >
-                  mdi-eye-outline
-                </v-icon>
-                <v-icon v-else>
-                  mdi-eye-off-outline
-                </v-icon>
-              </v-list-item-action>
-            </v-list-item>
-
-          </v-list-group>
-        </v-list>
-
-      </v-navigation-drawer>
+      <GraphEditor
+        :activeModels=activeModels
+        :hideAll=hideAll
+        :showAll=showAll
+        :collapseAll=collapseAll
+        :expandAll=expandAll
+        :inactiveColor=inactiveColor
+        :loaded=loaded
+      />
       <Graph
         class="graph"
         :nodes=nodes
@@ -134,13 +42,11 @@
   .graph {
     height: 100vh;
   }
-  .menu-model {
-    padding-left: 52px;
-  }
 </style>
 
 <script>
 import Graph from "./Graph.vue";
+import GraphEditor from "./GraphEditor.vue";
 
 
 const getColor = (index, numColors) => `hsl(${index * (360 / numColors)},50%,85%)`;
@@ -191,7 +97,7 @@ const abstractModelNode = (app, model, softColor, hardColor) => {
 
 export default {
   name: 'App',
-  components: {Graph},
+  components: {Graph, GraphEditor},
   props: ['abstractModels', 'models', 'connections'],
   methods: {
     showAll: function(ev) {
@@ -296,7 +202,6 @@ export default {
       inactiveColor: 'rgba(0, 0, 0, 0.54)',
       loaded,
       edges,
-      sidebar: false,
     };
   },
   computed: {
