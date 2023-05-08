419e2f782c37f92e87d5f72adecf6b5f1fd42c15
Charlie Denton <charlie@meshy.co.uk>
2023-05-08 13:25:39 +0100

    Remove old-Django compatible code

--- tests/urls.py
+++ tests/urls.py
@@ -1,13 +1,6 @@
+from django.urls import path
+
 from schema_graph.views import Schema
 
 
-try:
-    # Django 2+:
-    from django.urls import path
-
-    urlpatterns = [path("", Schema.as_view())]
-except ImportError:
-    # Django < 2:
-    from django.conf.urls import url
-
-    urlpatterns = [url(r"^$", Schema.as_view())]
+urlpatterns = [path("", Schema.as_view())]
